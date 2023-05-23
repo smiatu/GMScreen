22a53a76bb58dd0223c1d8d897c2b674b7dce8dc
smiatu <psmiatek@gmail.com>
2023-05-23 16:19:02 +0200

    Add scss and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./components/Home/Home";
+import Calendar from "./components/Calendar/Calendar";
+import Links from "./components/Links/Links"
+import Roller from "./components/Roller/Roller";
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <nav><div>navbar placeholder</div></nav>
+      <nav>
+        <div>navbar placeholder</div>
+        <Link to="/">Home</Link>
+        <Link to="calendar">Calendar</Link>
+        <Link to="links">Links</Link>
+        <Link to="roller">Roller</Link>
+      </nav>
       <header className="App-header">
         <h1>Ekran Mistrza Gry</h1>
       </header>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="calendar" element={ <Calendar/> } />
+        <Route path="links" element={ <Links/> } />
+        <Route path="roller" element={ <Roller/> } />
+      </Routes>
       <footer>
         Magia była kiedyś nielegalna, a teraz jest legalna. Spaczeń jest nielegalny, ale może kiedyś też stanie się legalny. Obecnie nie może być używany, chyba że w służbie cesarskiej, cokolwiek to znaczy. Prawo stosuje się jedynie po to, aby przynosiło korzyść prawodawcom. Jeżeli istnieje prawo, które czegoś zakazuje, zawsze zadaję sobie pytanie: "Kto je uchwalił?" Nigdy nie myl prawa ze sprawiedliwością, to zupełnie odrębne sprawy.
         David Ferring, Zrodzony z cienia
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Home placeholder
+    </div>
+  )
+}
+
+export default Home
