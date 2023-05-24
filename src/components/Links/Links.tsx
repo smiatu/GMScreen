f446e38c82d2d0f04f36750114af1fc7bcdb74f1
smiatu <psmiatek@gmail.com>
2023-05-24 18:15:19 +0200

    Add links and (very) basic roller logic

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -4,6 +4,13 @@ const Links = () => {
   return (
     <div>
       Links placeholder
+      <ul>
+        <li>Old World map: http://www.gitzmansgallery.com/shdmotwow-full.html</li>
+        <li>Military ranks of the German Empire: https://en.m.wikipedia.org/wiki/Military_ranks_of_the_German_Empire?fbclid=IwAR20t6LbvCCF5i_AjmkLIkCO17rt3K9vDcuxguyGaNwDGsK2cLy4f65GneU</li>
+        <li>Warhammer wiki: https://warhammer.fandom.com/pl/wiki/Warhammer_Wiki_pl</li>
+        <li>Khazalid translator: https://lingojam.com/Khazalid%28Warhammerdwarvish%29?fbclid=IwAR3z2LOGwYOUPmuau0TixhAmSPSFKPi_YgG7VfiPY_6KR9kDL1HmfHcAvfo</li>
+        <li>Calendar: https://warhammer-oko-za-oko.obsidianportal.com/wikis/kalendarz-imperium-ic?fbclid=IwAR00ZTijPM56PcOeiyaAADf0VHNUCeFt-WXTgZicTbyjk-HVBesS5ORFfK0</li>
+      </ul>
     </div>
   )
 }
--- src/components/Roller/Roller.tsx
+++ src/components/Roller/Roller.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Roller = () => {
+  const roll100 = () => {
+    setD100(Math.floor(Math.random() * 100));
+  }
+  const roll10 = () => {
+    setD10(Math.floor(Math.random() * 10));
+  }
+  const roll6 = () => {
+    setD6(Math.floor(Math.random() * 6 + 1));
+  }
+  const [d100, setD100] = useState(0);
+  const [d10, setD10] = useState(0);
+  const [d6, setD6] = useState(0);
+  
   return (
     <div>
-      Roller placeholder
+      <title>WFRP dice roller</title>
+      <div>
+        <div>
+          Roll d100:
+          <button onClick={roll100}>!</button>
+          { d100 }
+        </div>
+        <div>
+          Roll d10:
+          <button onClick={roll10}>!</button>
+          { d10 }
+        </div>
+        <div>
+          Roll d6:
+          <button onClick={roll6}>!</button>
+          { d6 }
+        </div>
+      </div>
     </div>
   )
 }
